a5bc61b1ec356016db19930acb48a573d41adea5
faz379 <fazelianpramudia@gmail.com>
2024-09-02 02:29:41 +0700

    perhitungan fazelian

---
+++ perhitungan_1.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 12;
+$b = 8;
+$c = 3;
+$d = 2;
+$e = 1;
+
+$hasil = (($a + $b)/($c + $d));
+
+echo "(($a + $b)/($c + $d)) - $e <br>";
+echo "hasil = $hasil";
+
+?>
---
+++ perhitungan_2.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 6;
+$b = 1;
+$c = 2;
+$d = 2.5;
+$e = 3;
+$f = 4;
+$g = 4.0;
+$h = 5;
+
+$penjumlahan = $a + $b;
+$pegurangan = $a - $b;
+$perkalian1 = $a * $c;
+$perkalian2 = $a * $d;
+$pembagian1 = $a / $f;
+$pembagian2 = $a / $g;
+$pembagian3 = $a % $h;
+$pembagian4 = $a % $f;
+$pembagian5 = $a % $e;
+
+
+echo "Penjumlahan dan Pengurangan : <br>";
+echo "$a + $b = $penjumlahan <br>";
+echo "$a - $b = $pegurangan";
+
+echo "<br><br>Perkalian : <br>";
+echo "$a * $c = $perkalian1 <br>";
+echo "$a * $d = $perkalian2";
+
+echo "<br><br>Pembagian : <br>";
+echo "$a / $f = $pembagian1 <br>";
+echo "$a / $g = $pembagian2 <br>";
+echo "$a % $h = $pembagian3 <br>";
+echo "$a % $f = $pembagian4 <br>";
+echo "$a % $e = $pembagian5";
+
+?>
