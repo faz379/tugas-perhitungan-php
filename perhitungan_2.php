831e13c518e05a95dd0d3cafe11c85ef47e0cd17
faz379 <fazelianpramudia@gmail.com>
2024-09-03 03:32:37 +0700

    perbaikan-perhitungan_2

--- perhitungan_2.php
+++ perhitungan_2.php
@@ -6,7 +6,6 @@ $c = 2;
 $d = 2.5;
 $e = 3;
 $f = 4;
-$g = 4.0;
 $h = 5;
 
 $penjumlahan = $a + $b;
@@ -14,7 +13,6 @@ $pegurangan = $a - $b;
 $perkalian1 = $a * $c;
 $perkalian2 = $a * $d;
 $pembagian1 = $a / $f;
-$pembagian2 = $a / $g;
 $pembagian3 = $a % $h;
 $pembagian4 = $a % $f;
 $pembagian5 = $a % $e;
@@ -30,7 +28,7 @@ echo "$a * $d = $perkalian2";
 
 echo "<br><br>Pembagian : <br>";
 echo "$a / $f = $pembagian1 <br>";
-echo "$a / $g = $pembagian2 <br>";
+echo ("$a / " . number_format($f, 1) . " = $pembagian1 <br>");
 echo "$a % $h = $pembagian3 <br>";
 echo "$a % $f = $pembagian4 <br>";
 echo "$a % $e = $pembagian5";
