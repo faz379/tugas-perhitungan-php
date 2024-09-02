ec133c8e1032e76fabfd6580e9fd06407c2d4407
faz379 <fazelianpramudia@gmail.com>
2024-09-03 03:46:00 +0700

    fix-bgt-perhitungan_2

--- perhitungan_2.php
+++ perhitungan_2.php
@@ -6,16 +6,16 @@ $c = 2;
 $d = 2.5;
 $e = 3;
 $f = 4;
-$h = 5;
+$g = 5;
 
 $penjumlahan = $a + $b;
 $pegurangan = $a - $b;
 $perkalian1 = $a * $c;
 $perkalian2 = $a * $d;
 $pembagian1 = $a / $f;
-$pembagian3 = $a % $h;
-$pembagian4 = $a % $f;
-$pembagian5 = $a % $e;
+$pembagian2 = $a % $g;
+$pembagian3 = $a % $f;
+$pembagian4 = $a % $e;
 
 
 echo "Penjumlahan dan Pengurangan : <br>";
@@ -29,8 +29,8 @@ echo "$a * $d = $perkalian2";
 echo "<br><br>Pembagian : <br>";
 echo "$a / $f = $pembagian1 <br>";
 echo ("$a / " . number_format($f, 1) . " = $pembagian1 <br>");
-echo "$a % $h = $pembagian3 <br>";
-echo "$a % $f = $pembagian4 <br>";
-echo "$a % $e = $pembagian5";
+echo "$a % $g = $pembagian2 <br>";
+echo "$a % $f = $pembagian3 <br>";
+echo "$a % $e = $pembagian4";
 
 ?>
